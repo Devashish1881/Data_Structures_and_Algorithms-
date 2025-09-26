6fa411f77e0305aa1fa7722b8227275897a87651
Devashish <125045160+Devashish1881@users.noreply.github.com>
2025-09-26 17:19:08 +0530

    Count of Matches in Tournament

---
+++ Math/Count of Matches in Tournament.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numberOfMatches(int n) 
+    {
+        if(n==1)
+        {
+            return 0;
+        }
+        int ans=0;
+      while(n!=1)
+      {
+          if(n%2==0)
+          {
+             ans=ans+n/2;
+             n=(int)n/2;
+          }
+          else
+          {
+             ans=ans+(int)(n-1)/2 ;
+             n=(int)n/2 +1;
+          }
+       } 
+       return ans;   
+    }
+}
